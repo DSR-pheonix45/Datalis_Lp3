b77b3d6ae943451010c556c4e18d1b6e34f2e50f
DSR-pheonix45 <imperialion45@gmail.com>
2025-05-31 20:19:45 +0530

    Fix: Update main.jsx for @vercel/analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Analytics } from "@vercel/analytics/next"
 import './index.css';
 import { Analytics } from "@vercel/analytics/react"
 
